1e22604c889ab7fce910cffe2542f7e58f33c5b6
katapi <hkatapi@gmail.com>
2025-12-10 12:04:04 +0900

    Manual sync from local monorepo

--- app/reservation/complete_backup/page.tsx
+++
@@ -1,7 +0,0 @@
-
-// Minimal reproduction test
-export const dynamic = 'force-dynamic';
-
-export default async function ReservationCompletePage() {
-    return <h1>Hello World - Build Test</h1>;
-}
